b916e931ae285eb21f20f743b23401d431380dd5
OliverKuhnNbg <oliver.o.kuhn@gmail.com>
2026-09-21 12:21:16 +0200

    useCase 1: init project and ddd design for componets

---
+++ erp-system/src/app/domains/inventory/data-access/inventory.model.ts
@@ -0,0 +1,7 @@
+// Domäne: Inventory
+export interface InventoryItem {
+  productId: string;
+  name: string;
+  stock: number;
+  cost: number;
+}
---
+++ erp-system/src/app/domains/sales/data-access/sales.model.ts
@@ -0,0 +1,7 @@
+// Domäne: Sales
+export interface SaleRecord {
+  id: string;
+  amount: number;
+  date: string;
+  productId: string;
+}
